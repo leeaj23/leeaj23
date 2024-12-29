053bc67e910f9ae2c846fca5350726a112844ecb
Amy Lee <amy.jeeyoon.lee@gmail.com>
2024-12-28 17:24:33 -0800

    home page!

--- app/page.js
+++ app/page.js
@@ -1,21 +1,24 @@
 import styles from "./output.css";
-import { Box, Container, CssBaseline, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardMedia, Container, CssBaseline, Typography } from "@mui/material";
 import NavBar from './components/NavBar';
 
 export default function Home() {
   return (
     <CssBaseline enableColorScheme>
       <NavBar></NavBar>
-      <Container maxWidth='lg' component='main' sx={{display: 'flex', flexDirection: 'column', my: 16, gap: 2,  border: 'solid black 2px'}}>
-        <Typography variant="h3">Amy Lee</Typography>
+      <Container maxWidth='lg' component='main' sx={{display: 'flex', flexDirection: 'column', my: 16, gap: 2}}>
+        <Box sx={{display: 'flex', flexDirection: 'row', gap: 2}}>
+          <Typography variant="h3">Hello, I'm Amy Lee</Typography>
+          <Avatar alt='hand waving' src='../wave.png' variant="square" sx={{width: 60, height: 60}}/>
+        </Box>
         <Typography>Seattle, WA</Typography>
-        <Container maxWidth='lg' component='main' sx={{display: 'flex', flexDirection: 'row', my: 4, gap: 4, border: 'solid black 2px'}}>
-          <Typography>Hello! My name is Amy and I am a student studying computer science at the University of Washington.</Typography>
-          <Box
-            component='img'
-            sx={{height: 500, width: 500, maxHeight: { xs: 500, md: 300 }, maxWidth: { xs: 500, md: 300 },}}
-            alt='Amy Lee with a Kirby shaped like a car on her head'
-            src="./components/headshot.png"/>
+        <Container maxWidth='lg' component='main' sx={{display: 'flex', flexDirection: 'row', my: 2, gap: 4}} disableGutters>
+          <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+            <Typography>I am an Computer Science undergrad at the University of Washington, where I've been fortunate enough to be recognized on the Dean's List every quarter (so far!). My academic journey is guided by a fascination towards AI and Data Science and my passion for learning.</Typography>
+            <Typography>Collaboration is at the heart of my work. I’ve loved teaming up with peers to tackle complex technical challenges, whether in projects or at hackathons. These experiences taught me how to break down big ideas into actionable steps and deliver practical, impactful solutions. Looking ahead, I’m excited to bring my analytical mindset, adaptability, and technical skills into software engineering. Whether it’s creating efficient algorithms, debugging tricky code, or building intuitive user experiences, I’m eager to contribute and learn.</Typography>
+            <Typography>TLDR; I’m someone who loves solving problems, sharing ideas, and turning curiosity into action. Feel free to reach out if you’d like to connect—I’d love to chat!</Typography>
+          </Box>
+          <Avatar alt='Amy Lee' src='../headshot.png' variant='square' sx={{width: 400, height: 400, borderRadius: 5}}/>
         </Container>
       </Container>
     </CssBaseline>
