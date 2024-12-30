76e744a3c7e66cfc91b80c6a23b8373bd03e7816
Amy Lee <amy.jeeyoon.lee@gmail.com>
2024-12-30 08:55:10 -0800

    updated blurbs

--- app/projects/page.js
+++ app/projects/page.js
@@ -53,7 +53,7 @@ const projectData = [
         img: "../flightapp.png",
         title: "Flight Tracker Application",
         type: "CSE 344 Capstone Project",
-        blurb: "Developed a tailored database system to manage user data and facilitate flight scheduling through a command-line interface. Built using Java and SQL, this database is hosted in Microsoft Azure. To safeguard sensitive data, I implemented robust security protocols including encryption, hashing, salting, and identity management to ensure data confidentiality without compromising the system's performance, allowing for effecient and secure query processing.",
+        blurb: "Designed and implemented a database system to manage user data and streamline flight scheduling via a command-line interface. Built with Java and SQL, the system is hosted on Microsoft Azure. Integrated encryption, hashing, salting, and identity management to secure sensitive data, ensuring efficient and reliable query processing without compromising performance.",
         dates: "December 2024",
         links: [
             {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/flightapp"}
@@ -64,7 +64,7 @@ const projectData = [
         img: "../portfolio.png",
         title: "Personal Website",
         type: "Personal Project",
-        blurb: "Personal website designed to showcase projects, experiences, and personality. Serves as a digital portfolio, allowing me to document my journey as a software engineer and see how far I've come.",
+        blurb: "Developed a personal website to highlight projects, experiences, and growth as a software engineer. Acts as a dynamic portfolio to document progress and showcase achievements.",
         dates: "November 2024 - Present",
         links: [
             {"avatar": "../../link.png", "name": "link", "link": "/"}, //FIX CHANGE THE LINK WHEN UR ACTUALLY DOING THE THING
@@ -76,7 +76,7 @@ const projectData = [
         img: "../circles.jpg",
         title: "Circles",
         type: "DubHacks 2024 Project",
-        blurb: "In celebration of the 10th anniversary of DubHacks and its theme, \"How Far We've Come, How Far We'll Go,\" we proudly introduce Circles, a dating platform that connects the past with the future. Circles is designed to spark genuine connections by rekindling shared childhood memories and experiences, whether it's the schools they attended or the movies they loved growing up. Our platform uses AI to suggest matches based on these unique connections, fostering conversations through tailored prompts that lead to more natural and less awkward interactions. By building relationships rooted in shared experiences, we pave the way for meaningful connections that can flourish over time.",
+        blurb: "Developed Circles, a dating platform celebrating DubHacks' 10th anniversary theme: \"How Far We've Come, How Far We'll Go.\" Circles connects users through shared childhood memories and experiences, such as schools attended or favorite movies. Leveraging AI, the platform suggests matches based on these unique ties and provides personalized prompts to encourage natural, meaningful interactions. Focused on building connections rooted in familiarity, Circles fosters relationships designed to grow over time.",
         dates: "October 2024",
         links: [
             {"avatar": "../../link.png", "name": "link", "link": "https://youtu.be/6iMnwmW5kxI"},
@@ -88,7 +88,7 @@ const projectData = [
         img: "../genone.png",
         title: "GenOne",
         type: "HeadstarterAI 2024 Project",
-        blurb: "Oversaw team of 4 additional developers from HeadstarterAI Fellowship to build an application to provide students with links to job postings, alumni contacts, and career resources. Created a landing page and contacted school alumnus and curated a database of willing mentors for university students. Implemented searching, sorting, and filtering methods to ensure easy access for all resources provided.",
+        blurb: "Led a team of four developers during the HeadstarterAI Fellowship to create an application connecting students with job postings, alumni contacts, and career resources. Designed a landing page, curated a database of mentors, and established outreach with school alumni. Implemented advanced search, sorting, and filtering functionality to ensure seamless access to all resources.",
         dates: "July 2024 - September 2024",
         links: [
             {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/fli-app"}
@@ -99,7 +99,7 @@ const projectData = [
         img: "../inventory.png",
         title: "Inventory Management System",
         type: "HeadstarterAI 2024 Project",
-        blurb: "Created a basic inventory management system for the HeadstarterAI 2024 Fellowship. Users can input the ingredients they have, incrementing and decrementing their values to match the quantity of items they have in their pantries. Users can also search for their wanted items, viewing the quantities of supplies they currently have.",
+        blurb: "Developed an inventory management system for the HeadstarterAI 2024 Fellowship. The application allows users to track pantry items by adding, adjusting, and viewing quantities. Included search functionality to enable users to quickly locate and review their supplies.",
         dates: "July 2024",
         links: [
             {"avatar": "../../link.png", "name": "link", "link": "/"}, //FIX CHANGE THE LINK WHEN UR ACTUALLY DOING THE THING
@@ -111,7 +111,7 @@ const projectData = [
         img: "../umessage.png",
         title: "uMessage",
         type: "CSE 332 Project",
-        blurb: "uMessage is a messaging platform that UW students can utilize, featuring standard text auto-complete and word suggestions that are implemented using AVL-Trees, B-Trees, Heaps, Tries, and Dictionaries developed from scratch. Users are able to store and retrieve their chat logs with bots and other users using a data-persistent database, hosted on Google Firebase.",
+        blurb: "Designed a messaging platform for UW students with features like text auto-complete and word suggestions, implemented using custom-built AVL Trees, B-Trees, Heaps, Tries, and Dictionaries. Integrated a data-persistent database hosted on Google Firebase, enabling users to store and retrieve chat logs with bots and other users.",
         dates: "April 2024",
         links: [
             {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/uMessage"}
@@ -122,7 +122,7 @@ const projectData = [
         img: "../flashcard.png",
         title: "Flashcard Application",
         type: "CSE 331 Project",
-        blurb: "Designed a full-stack application that allows users to create, view, practice, and test with flashcard decks, ensuring data persistence across page reloads. Enabled users to track their practice scores, view all past scores, and analyze their mistakes, ensuring accurate and user-friendly performance tracking.",
+        blurb: "Developed a full-stack application for creating, viewing, practicing, and testing with flashcard decks. Implemented data persistence to maintain functionality across page reloads. Added features for tracking practice scores, reviewing past performance, and analyzing mistakes, ensuring accurate and intuitive performance insights.",
         dates: "March 2024",
         links: [
             {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/flashcards"}
@@ -133,7 +133,7 @@ const projectData = [
         img: "../caffiend.png",
         title: "Caf-Fiend",
         type: "DubHacks 2023 Project",
-        blurb: "Caf-Fiend is your ultimate caffeine companion where you can seamlessly input your caffeine intake to help you manage and monitor your daily caffeine consumption with precision. Caf-Fiend not only provides real-time tracking but also offers personalized guidance, ensuring that you stay well within recommended daily caffeine limits for a healthier, more balanced lifestyle. Beyond daily tracking, Caf-Fiend lets you log your caffeine intake by week, month, and year, granting you invaluable insights into your long-term health and wellness patterns.",
+        blurb: "Created Caf-Fiend, a caffeine management application designed for precise tracking and monitoring of daily intake. The app offers personalized guidance to help users stay within recommended caffeine limits for a healthier lifestyle. Extended functionality includes logging and reviewing caffeine consumption trends over weeks, months, and years, providing valuable insights into long-term health patterns.",
         dates: "October 2023",
         links: [
             {"avatar": "../../link.png", "name": "link", "link": "https://gordondhuang.github.io/Caf-Fiend/index.html"}, 
@@ -145,7 +145,7 @@ const projectData = [
         img: "../huffman.png",
         title: "Huffman Tree",
         type: "CSE 143 Project",
-        blurb: "Developed a Huffman Tree using priority queues and binary trees for efficient text file compression based on character frequency. Created methods to encode text files into compressed binary files and decode them back to their original form using Huffman coding.",
+        blurb: "Implemented a Huffman Tree using priority queues and binary trees to enable efficient text file compression based on character frequency. Designed methods to encode text files into compressed binary formats and decode them back to their original state using Huffman coding principles.",
         dates: "November 2023",
         links: [
             {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/cse143homeworks"}
@@ -156,10 +156,10 @@ const projectData = [
         img: "../tetris.png",
         title: "Tetris Clone",
         type: "Personal Project",
-        blurb: "Created a Tetris clone using Unity. Users can play the standard game of Tetris, containing specialized levels and colors once the user surpasses level 13. Custom blocks, sounds, and instructions are included in the game.",
+        blurb: "Developed a Tetris clone in Unity, featuring the classic gameplay along with specialized levels and color schemes after reaching level 13. The game includes custom blocks, sounds, and instructions, providing a personalized and engaging experience for users.",
         dates: "July 2022",
         links: [
-            {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/portfolio"} //FIX CHANGE THE LINK WHEN UR ACTUALLY DOING THE THING
+            {"avatar": "../../github.png", "name": "github", "link": "https://github.com/leeaj23/tetris"} //FIX CHANGE THE LINK WHEN UR ACTUALLY DOING THE THING
         ],
         skills: ["Unity", "C#"]
     },
